b4bbfc790e56dd097084d47fc52584b3a638515f
hackieLabs <129271681+hackieLabs@users.noreply.github.com>
2023-07-04 14:59:16 +0000

    name changed

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Hackie | Nextjs starter",
+  name: "Hackie | Nextjs",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
   mainNav: [
